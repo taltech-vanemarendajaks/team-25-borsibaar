103c7afa0727672d692a8f429686214e6d6b1149
Tomi Markus Alber <tomimarkusalber@gmail.com>
2026-01-29 11:32:42 +0200

    chore: add prettier config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
+import prettierConfig from 'eslint-plugin-prettier/recommended';
  
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-    {
+  prettierConfig,
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
