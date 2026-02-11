ca18f11ef3d09404ca76bd02d7938716be3916ac
Tomi Markus Alber <tomimarkusalber@gmail.com>
2026-02-11 20:45:46 +0200

    chore: standardize linting workflow (#41)
    * chore: upgrade eslint version
    
    * chore: remove legacy @eslint/eslintrc package
    
    * refactor: legacy eslint config
    
    * fix: calling impure function during render
    
    * fix: error being undefined type
    
    * perf: change img tag to next/image
    
    * chore: add eslint typescript config
    
    * chore: turn off no any rule in eslint config
    
    * chore: add prettier config
    
    * chore: add lint-staged with husky
    
    * chore: add .next to prettierignore list

---
+++ frontend/.lintstagedrc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path");
+
+const buildEslintCommand = filenames =>
+  `eslint --fix ${filenames.map(f => `"${path.relative(process.cwd(), f)}"`).join(" ")}`;
+
+module.exports = {
+  "*.{ts,tsx}": [buildEslintCommand],
+};
--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+ 
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettierConfig,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
-];
-
-export default eslintConfig;
+  globalIgnores([
+    "node_modules/**",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+ 
+export default eslintConfig
